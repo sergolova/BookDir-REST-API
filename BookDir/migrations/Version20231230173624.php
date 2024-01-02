a172de1ee3ea3d24c89a2097bd12b1d6814cafb9
sergolova <sergolova666@tutanota.com>
2024-01-02 15:46:47 +0200

    BD-003 - add demo migration

--- BookDir/migrations/Version20231230173624.php
+++ BookDir/migrations/Version20231230173624.php
@@ -14,7 +14,7 @@ final class Version20231230173624 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create Books and Authors tables';
     }
 
     public function up(Schema $schema): void
